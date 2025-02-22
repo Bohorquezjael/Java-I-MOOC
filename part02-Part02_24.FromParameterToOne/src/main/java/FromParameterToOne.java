afdc7270d6479b542edca590c3f49d19cebed56b
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-22 06:29:05 -0600

    add: part02-Part02_24.FromParameterToOne

---
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -0,0 +1,16 @@
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+        printFromNumberToOne(5);
+
+    }
+
+    public static void printFromNumberToOne(int i) {
+        for (int j = i; j > 0; j--) {
+            System.out.println(j);
+        }
+    }
+
+}
