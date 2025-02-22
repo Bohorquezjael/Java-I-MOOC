282c07c630774505fa481a0f4a4b39d9a5dcae64
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-22 06:32:14 -0600

    add: part05-Part05_15.Archive

---
+++ part05-Part05_15.Archive/src/main/java/Item.java
@@ -0,0 +1,39 @@
+
+import java.util.Objects;
+
+public class Item {
+    private String identifier;
+    private String name;
+    
+    public Item(String identifier, String name){
+        this.identifier = identifier;
+        this.name = name;
+    }
+    
+    public String getIdentifier() {
+        return this.identifier;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (!Objects.equals(this.identifier, other.identifier)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
