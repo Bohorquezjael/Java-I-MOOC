4faa8db5c84ce7320a49313a1ef8e456b53022f8
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-22 06:31:49 -0600

    add: part05-Part05_03.Cube

---
+++ part05-Part05_03.Cube/src/main/java/Cube.java
@@ -0,0 +1,18 @@
+public class Cube {
+    private int length;
+
+    public Cube(int edgeLength) {
+        this.length = edgeLength;
+    }
+    
+    public int volume(){
+        return (int) Math.pow(this.length, 3);
+    }
+
+    @Override
+    public String toString() {
+        return "The length of the edge is " + this.length + " and the volume " + this.volume();
+    }
+    
+    
+}
